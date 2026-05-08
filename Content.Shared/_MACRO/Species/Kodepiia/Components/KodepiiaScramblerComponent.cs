53a138d47a09c04382dae8281ab1e34c9c0514d8
starlighthowls <starlighthowls7@gmail.com>
2026-05-08 10:15:24 +0100

    actions

---
+++ Content.Shared/_MACRO/Species/Kodepiia/Components/KodepiiaScramblerComponent.cs
@@ -0,0 +1,18 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._MACRO.Species.Kodepiia.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class KodepiiaScramblerComponent : Component
+{
+    [DataField]
+    public EntityUid? ScramblerAction;
+
+    [DataField]
+    public string? ScramblerActionId = "ActionKodepiiaScrambler";
+
+    [DataField]
+    public SoundSpecifier ScramblerSound = new SoundPathSpecifier("/Audio/_MACRO/Kodepiia/kodescramble/kodescramble.ogg");
+
+}
